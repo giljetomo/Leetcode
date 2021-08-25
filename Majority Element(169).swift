d9a8bbd76d5b41ea15339f292daf244323ac423b
Macbook Pro <gmjetomo@outlook.com>
2021-08-25 16:15:42 -0700

    added problem #169

---
+++ Majority Element(169).swift
@@ -0,0 +1,12 @@
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        
+        var dict = [Int: Int]()
+        for num in nums {
+            dict[num, default: 0] += 1
+            if dict[num]! > nums.count / 2 { return num }
+        }
+        
+        return nums.first!
+    }
+}
